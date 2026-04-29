c2e2716a37f3cc9f738d12329f9ae60650073b04
Cameron Hendrikse <20152311@tafe.wa.edu.au>
2026-04-29 19:23:33 +0800

    refactor(project): modularize TikTakToe structure

---
+++ game/routes.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template, redirect, url_for
+
+from game.logic import check_winner, check_draw
+
+game_routes = Blueprint('game_routes', __name__)
+
+# Initialise game board and current player
+board = [' '] * 9
+current_player = 'X'
+
+
+@game_routes.route('/')
+def index():
+    winner = check_winner(board)
+    draw = check_draw(board)
+    return render_template('index.html', board=board, current_player=current_player, winner=winner, draw=draw)
+
+
+@game_routes.route('/play/<int:cell>')
+def play(cell):
+    # breakpoint()
+    global current_player
+    if board[cell] == ' ':
+        board[cell] = current_player
+        if not check_winner(board):
+            current_player = 'O' if current_player == 'X' else 'X'
+    return redirect(url_for('game_routes.index'))
+
+
+@game_routes.route('/reset')
+def reset():
+    global board, current_player
+    board = [' '] * 9
+    current_player = 'X'
+    return redirect(url_for('game_routes.index'))
